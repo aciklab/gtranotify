637c80c7c7041e95294694699975b2f5e38d3e96
Ali Orhun Akkirman <divercans@gmail.com>
2017-09-22 16:41:52 +0300

    Create gtranotify.py

---
+++ gtranotify.py
@@ -0,0 +1,29 @@
+#/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import os
+import gi
+import sys
+
+# regulator
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+# config translator
+from translate import Translator
+translator= Translator(to_lang="tr")
+
+# check notify
+gi.require_version('Notify', '0.7')
+from gi.repository import Notify
+
+# text to translate
+text = os.popen('xsel').read()
+
+# get translated text
+result = translator.translate(text)
+
+# notify text
+Notify.init("translate")
+trlated = Notify.Notification.new(str(text), str(result))
+trlated.show ()
